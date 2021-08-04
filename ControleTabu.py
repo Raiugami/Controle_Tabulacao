68b7a812ce85f3c265c61e06e7bad8bf65cb2bd5
Gabriel Aguiar <72907265+Raiugami@users.noreply.github.com>
2021-08-04 05:22:24 -0300

    Add files via upload

--- ControleTabu.py
+++ ControleTabu.py
@@ -5,9 +5,8 @@ print("\n -=- -=- -=- -=- -=- -=- -=- TABULAÇÂO ENCAMINHAMENTO-=- -=- -=- -=- 
 
 while True:
 
-    arquivo = open("TMA2.txt", "a")
+    arquivo = open("Tabulação.txt", "a")
     problema = int(input(''' Escolha o problema:  
-
     [1] SD - Aplicativo com Problema
     [2] SD - Bitlocker
     [3] SD - CPU Problema
@@ -19,18 +18,98 @@ while True:
     [9] SD - Windows - Sistema Operacional
     [10] TS - VDI problema
     [11] TSWVD - VDI problema
-
-
     informe aqui:  '''))
+
+    operacao = int(input(''' Escolha a operação:  
+
+        [1] Aliança
+        [2] Banco Pan
+        [3] Bradesco Affinity
+        [4] Conectcar
+        [5] CTF
+        [6] ENEL - RJ
+        [7] Intermédica PME / Interodonto IOD
+        [8] Kroton
+        [9] Lenovo
+        [10] Marisa
+        [11] Motorola
+        [12] OI
+        [13] PASA
+        [14] Sem Parar
+        [15] Shell
+        [16] Unimed - RJ
+        [17] Via Varejo
+        [18] VIVO
+        [19] White Martins 
+
+        informe aqui:  '''))
+
+    if operacao == 1:
+        print("Operação : Aliança")
+        arquivo.write("\nOperação : Aliança")
+    elif operacao == 2:
+        print("Operação : Banco PAN")
+        arquivo.write("\nOperação : Banco PAN")
+    elif operacao == 3:
+        print("Operação : Bradesco Affinity")
+        arquivo.write("\nOperação : Bradesco Affinity")
+    elif operacao == 4:
+        print("Operação : Conectar")
+        arquivo.write("\nOperação : Conectar")
+    elif operacao == 5:
+        print("Operação : CTF")
+        arquivo.write("\nOperação : CTF")
+    elif operacao == 6:
+        print("Operação : ENEL - RJ")
+        arquivo.write("\nOperação : ENEL - RJ")
+    elif operacao == 7:
+        print("Operação : Intermédica PME / Interodonto IOD")
+        arquivo.write("\nOperação : Intermédica PME / Interodonto IOD")
+    elif operacao == 8:
+        print("Operação: Kroton")
+        arquivo.write("\nOperação : Kroton")
+    elif operacao == 9:
+        print("Operação : Lenovo")
+        arquivo.write("\nOperação : Lenovo")
+    elif operacao == 10:
+        print("Operação : Marisa ")
+        arquivo.write("\nOperação : Marisa")
+    elif operacao == 11:
+        print("Operação : Motorola")
+        arquivo.write("\nOperação : Motorola")
+    elif operacao == 12:
+        print("Operação : OI ")
+        arquivo.write("\nOperação : OI")
+    elif operacao == 13:
+        print("Operação : PASA ")
+        arquivo.write("\nOperação : PASA")
+    elif operacao == 14:
+        print("Operação : Sem Parar")
+        arquivo.write("\nOperação : Sem Parar")
+    elif operacao == 15:
+        print("Operação : Shell")
+        arquivo.write("\nOperação : Shell")
+    elif operacao == 16:
+        print("Operação : Unimed - RJ")
+        arquivo.write("\nOperação : Unimed - RJ")
+    elif operacao == 17:
+        print("Operação : Via Varejo")
+        arquivo.write("\nOperação : Via Varejo")
+    elif operacao == 18:
+        print("Operação :VIVO")
+        arquivo.write("\nOperação :VIVO")
+    elif operacao == 19:
+        print("Operação :White Martins")
+        arquivo.write("\nOperação :White Martins")
+    else:
+        print("Nenhuma operação encontrada :(")
 
     if problema == 1:
         print("Problema: SD - Aplicativo com Problema")
         arquivo.write("\nProblema: SD - Aplicativo com Problema\n")
-
     elif problema == 2:
         print("Problema: SD - Bitlocker")
         arquivo.write("\nProblema: SD - Bitlocker\n")
-
     elif problema == 3:
         print("Problema: SD - CPU Problema")
         arquivo.write("\nProblema: SD - CPU Problema\n")
@@ -40,7 +119,6 @@ while True:
         arquivo.write("Horario: {}\n".format(numero))
         cep = str(input('\nInforme o CEP do colaborador: '))
         arquivo.write("Horario: {}\n".format(cep))
-
     elif problema == 4:
         print("Problema: SD - Headset Problema")
         arquivo.write("\nProblema: SD - Headset Problema\n")
@@ -50,7 +128,6 @@ while True:
         arquivo.write("Horario: {}\n".format(numero))
         cep = str(input('\nInforme o CEP do colaborador: '))
         arquivo.write("Horario: {}\n".format(cep))
-
     elif problema == 5:
         print("Problema: SD - Monitor Problema")
         arquivo.write("\nProblema: SD - Monitor Problema\n")
@@ -60,7 +137,6 @@ while True:
         arquivo.write("Horario: {}\n".format(numero))
         cep = str(input('\nInforme o CEP do colaborador: '))
         arquivo.write("Horario: {}\n".format(cep))
-
     elif problema == 6:
         print("Problema: SD - Mouse Problema")
         arquivo.write("\nProblema: SD - Mouse Problema\n")
@@ -70,7 +146,6 @@ while True:
         arquivo.write("Horario: {}\n".format(numero))
         cep = str(input('\nInforme o CEP do colaborador: '))
         arquivo.write("Horario: {}\n".format(cep))
-
     elif problema == 7:
         print("Problema: SD - Teclado Problema")
         arquivo.write("\nProblema: SD - Teclado Problema")
@@ -80,21 +155,57 @@ while True:
         arquivo.write("Horario: {}\n".format(numero))
         cep = str(input('\nInforme o CEP do colaborador: '))
         arquivo.write("Horario: {}\n".format(cep))
-
     elif problema == 8:
         print("Problema: SD - Windows - Falha de logon")
         arquivo.write("\nProblema: SD - Windows - Falha de logon")
-
     elif problema == 9:
         print("Problema: SD - Windows - Sistema Operacional")
         arquivo.write("\nProblema: SD - Windows - Sistema Operacional\n")
-
     elif problema == 10:
+        link = ""
+        if operacao == 1:
+            link: str = 'vdi-aliança.atento.com.br:'
+        elif operacao == 2:
+            link: str = 'vdi-bancopan.atento.com.br:'
+        elif operacao == 3:
+            link: str = 'vdi-bradesco.atento.com.br:'
+        elif operacao == 4:
+            link: str = 'vdi-conectcar.atento.com.br:'
+        elif operacao == 5:
+            link: str = 'vdi-ctf.atento.com.br:'
+        elif operacao == 6:
+            link: str = 'vdi-enel.atento.com.br:'
+        elif operacao == 7:
+            link: str = 'vdi-intermedica.atento.com.br:'
+        elif operacao == 8:
+            link: str = 'vdi-kroton.atento.com.br:'
+        elif operacao == 9:
+            link: str = 'vdi-lenovo.atento.com.br:'
+        elif operacao == 10:
+            link: str = 'vdi-marisa.atento.com.br:'
+        elif operacao == 11:
+            link: str = 'vdi-motorola.atento.com.br:'
+        elif operacao == 12:
+            link: str = 'vdi-oi.atento.com.br:'
+        elif operacao == 13:
+            link: str = 'vdi-pasa.atento.com.br:'
+        elif operacao == 14:
+            link: str = 'vdi-semparar.atento.com.br:'
+        elif operacao == 15:
+            link: str = 'vdi-shell.atento.com.br:'
+        elif operacao == 16:
+            link: str = 'vdi-unimed.atento.com.br:'
+        elif operacao == 17:
+            link: str = 'vdi-viavarejo.atento.com.br:'
+        elif operacao == 18:
+            link: str = 'vdi-vivo.atento.com.br:'
+        elif operacao == 19:
+            link: str = 'vdi-whitemartins.atento.com.br:'
+
         print("Problema: TS - VDI problema\n")
         arquivo.write("\nProblema: TS - VDI problema")
-        porta = str(input('\nInforme o endereço e porta da área remota: (ex:vdi-viavarejo.atento.com.br:3359) : '))
-        arquivo.write("Link: {}\n".format(porta))
-
+        porta = str(input('\nInforme a porta da área remota: '))
+        arquivo.write("\nPorta: {}{}\n".format(link, porta))
     elif problema == 11:
         print("Problema: TSVDI - WVD Microsoft\n")
         arquivo.write("\nProblema: TSWVD - VDI problema\n")
@@ -102,7 +213,6 @@ while True:
         arquivo.write("Link: {}\n".format(link))
         pool = str(input('\nInforme o pool da área remota: '))
         arquivo.write("Link: {}\n".format(pool))
-
     else:
         print("Opção invalida :( ")
 
@@ -121,9 +231,6 @@ while True:
     site = str(input('\nInforme o site do colaborador: '))
     arquivo.write("Site: {}\n".format(site))
 
-    problema = str(input('\nInforme a operação do colaborador: '))
-    arquivo.write("Operação: {}\n".format(problema))
-
     horario = str(input('\nInforme o horario de trabalho do colaborador: '))
     arquivo.write("Horario: {}\n".format(horario))
 
@@ -132,6 +239,6 @@ while True:
     print(" \n ======== Registrado :D ======== \n ")
     time.sleep(3)
 
-    arquivo.write('-=-' * 24)
+    arquivo.write('-=-'*24)
     arquivo.close()
-    os.system("notepad.exe TMA2.txt")
+    os.system("notepad.exe Tabulação.txt")
